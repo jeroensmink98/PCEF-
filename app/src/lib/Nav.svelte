dbdec2e3c41e980bd518aa92964b5c127e5a8be2
Jeroen Smink <jeroensmink98@gmail.com>
2023-01-03 09:12:32 +0700

    feat: Navigation panel

--- app/src/lib/Nav.svelte
+++ app/src/lib/Nav.svelte
@@ -1,1 +1,9 @@
-
+<div class="navigation">
+    <p>Navigation</p>
+    <a href="/">Home</a>
+    <hr>
+    <a href="/teacher">Teachers</a>
+    <a href="/book">Books</a>
+    <a href="/school">Schools</a>
+    <hr>
+</div>
