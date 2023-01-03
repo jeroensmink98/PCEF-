b0fe1aff447a80308092f35afa0056ee6cfb2a99
Jeroen Smink <jeroensmink98@gmail.com>
2023-01-03 14:22:37 +0700

    feat: refactor books

---
+++ app/src/lib/Book.ts
@@ -0,0 +1,12 @@
+import { pb } from "./Pocketbase";
+
+
+export async function fetchBooks(){
+    try{
+        const resultList = await pb.collection('book').getFullList();
+        return resultList;
+    }catch(err){
+        console.log(err);
+    }
+
+}
