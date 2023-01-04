2d14ae89a6b9caf98e6d38d0548496903f1fcfee
Jeroen Smink <jeroensmink98@gmail.com>
2023-01-04 09:38:58 +0700

    chore: sort records by level_name

--- app/src/lib/Level.ts
+++ app/src/lib/Level.ts
@@ -3,7 +3,9 @@ import {pb} from '$lib/Pocketbase';
 
 export async function fetchLevels(){
     try{
-        const resultList = await pb.collection('level').getFullList();
+        const resultList = await pb.collection('level').getFullList(200, {
+            sort: 'level_name'
+        });
         return resultList;
     }catch (err){
         console.log(err);
