3ddc28b4ccd4857efede79b8f96ef1bfd4a1fb35
matthh9797 <matthewh@netextremity.com>
2023-01-05 13:23:25 +0700

    Updated level table

--- app/src/lib/level/LevelList.svelte
+++ app/src/lib/level/LevelList.svelte
@@ -18,17 +18,36 @@
         const levels: any = await fetchLevels()
         LevelStore.set(levels)
     })
+
+    // css variables
+    let table_header = ""
+    let table_row = "border px-4 py-2 font-medium"
+    let delete_button = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-3 rounded"
     
 </script>
 <LevelCreate/>
 <hr>
 <div class="levels">
-    {#each $LevelStore as level }
         <div class="level">
-            <div class="level_name">
-                <p>name: <b>{level.level_name}</b> code: <b>{level.short_code}</b></p>
-                <button on:click={() => deleteLevel(level)}>Delete</button>
+            <div class="rounded-t-l overflow-hidden from-emerald-50 to-teal-100 p-3">
+                <table class="min-w-full">
+                <thead>
+                    <tr>
+                        <th class="{table_header}">Name</th>
+                        <th class="{table_header}">Code</th>
+                        <!-- No header for delete column -->
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each $LevelStore as level }
+                    <tr>
+                        <td class="{table_row}">{level.level_name}</td>
+                        <td class="{table_row}">{level.short_code}</td>
+                        <td><button class="{delete_button}" on:click={() => deleteLevel(level)}>Delete</button></td>
+                    </tr>
+                    {/each}
+                </tbody>
+                </table>
             </div>
         </div>
-    {/each}
 </div>
